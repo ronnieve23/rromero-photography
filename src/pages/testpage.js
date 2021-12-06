1c51b69775579301a4475983deda18d1c6ea55ba
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-12-06 08:19:03 -0500

    initial project files

---
+++ src/pages/testpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from 'react-bootstrap';
+
+
+const Test = () => {
+
+    return (
+        <Container>
+            <h1>TestPage</h1>
+        </Container>
+    )
+}
+
+export default Test;
