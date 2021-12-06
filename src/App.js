9c08f8eb12ed5b5bc8210098f0e6a964f256197b
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-12-06 13:07:31 -0500

    reverted changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Home from './pages/homepage';
 import Test from './pages/testpage';
@@ -9,13 +9,13 @@ import Navigation from './components/navbar';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
     <Navigation/>
-          <Routes>
-          <Route exact path="/rromero-photography/home" element={<Home/>} />
-          <Route exact path="/rromero-photography/test" element={<Test/>} />
+        <Routes>
+            <Route index path="/home" element={<Home/>} />
+            <Route path="/test" element={<Test/>} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
   );
 }
